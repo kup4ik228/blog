431f86e54cc6d154f09ce8c0459b5d4454a64923
kup4ik228 <kupcovila63@gmail.com>
2026-04-29 11:43:21 +0500

    4 работа

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -3,9 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use Sluggable;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable(): array
+    {
+        return[
+            'slug'=>[
+                'source'=>'title'
+            ]
+        ];
+    }
 }
